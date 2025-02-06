f52544e11e936b3d0fc55b219ca57f4c59996bf2
Alicia <alicia.ibanez.escudero@gmail.com>
2025-02-06 09:57:25 +0100

    Modificaciones en  archivo java y readme añadiendo mis datos. Añado al area de preparacion el archivo modificado"

--- App.java
+++ App.java
@@ -4,6 +4,11 @@ public class App {
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("Alicia Ibáñez Escudero");
+    System.out.println("IES San Andrés");
+     System.out.println("La Virgen del Camino");
+
+
   }
   
 }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # stunning-octo-eureka
+# Proyecto modificado por Alicia Ibáñez Escudero
 
 Aplicación Java sencilla para su uso en el módulo de "Entronos de desarrrollo".
 Ilustra el uso de git push entre otros.
